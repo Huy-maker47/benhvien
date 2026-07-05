abb446cf69f245172a26daf21efd6dd8c9a68345
Huy-maker47 <24DH112625@st.huflit.edu.vn>
2026-07-05 21:01:20 +0700

    Add project files.

---
+++ benhvien/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using benhvien.Data;
+using benhvien.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace benhvien.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DonationGuide()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string keyword)
+        {
+            var hospitals = _context.Hospitals.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                hospitals = hospitals.Where(h => h.Name.Contains(keyword));
+            }
+
+            return View(hospitals.ToList());
+        }
+    }
+}
---
+++ benhvien/Models/BloodRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace benhvien.Models
+{
+    public class BloodRequest
+    {
+        public int Id { get; set; }
+
+        public int HospitalId { get; set; }
+        
+        [ForeignKey(nameof(HospitalId))]
+        public virtual Hospital? Hospital { get; set; }
+
+        public string? BloodType { get; set; }
+
+        public int QuantityNeeded { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsUrgent { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
